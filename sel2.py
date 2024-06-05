d0e4b86712ef48ceda54e7c14e457e1ec7ecb02a
Fawad Ali <71928305+fawadss1@users.noreply.github.com>
2024-06-05 21:48:27 +0500

    Update sel2.py

--- sel2.py
+++ sel2.py
@@ -1,5 +1,4 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -12,14 +11,23 @@ try:
     driver.maximize_window()
     driver.get('https://freebitco.in')
 
-    loginbtn = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//li[contains(@class,'login_menu_button')]")))
-    loginbtn.click()
+    loginTab = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//li[contains(@class,'login_menu_button')]")))
+    loginTab.click()
     time.sleep(5)
     nothnaksBtn = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "(//div[@class='pushpad_deny_button'])[1]")))
     nothnaksBtn.click()
 
     emailfield = driver.find_element(By.XPATH, "//input[@name='btc_address']")
-    emailfield.send_keys('fawad.khan@suvastutech.com')
+    emailfield.send_keys('fawadstar449672@gmail.com')
+    passfield = driver.find_element(By.XPATH, "//input[@id='login_form_password']")
+    passfield.send_keys('Jk,FVD8t5vUfdtM')
+
+    loginBtn = driver.find_element(By.XPATH, "//button[@id='login_button']")
+    loginBtn.click()
+    time.sleep(20)
+    checkBtn = driver.find_element(By.XPATH, "//div[@id='checkbox']")
+    checkBtn.click()
+
 
 finally:
     pass
